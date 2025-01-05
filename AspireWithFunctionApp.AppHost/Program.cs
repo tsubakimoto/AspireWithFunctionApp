4f43cdc66742febfe6d83ff85620a99e14f2f684
Yuta Matsumura <tkmbmbr@gmail.com>
2025-01-05 21:53:45 +0900

    add azure integrations

--- AspireWithFunctionApp.AppHost/Program.cs
+++ AspireWithFunctionApp.AppHost/Program.cs
@@ -9,6 +9,17 @@ var blobs = storage.AddBlobs("blobs");
 var queues = storage.AddQueues("queues");
 var tables = storage.AddTables("tables");
 
+var secrets = builder.AddAzureKeyVault("secrets");
+
+var openai = builder.AddAzureOpenAI("openai");
+
+var cosmos = builder.AddAzureCosmosDB("cosmos");
+var cosmosdb = cosmos.AddDatabase("mydb");
+
+var search = builder.AddAzureSearch("search");
+
+var webPubSub = builder.AddAzureWebPubSub("wps");
+
 // https://learn.microsoft.com/ja-jp/dotnet/aspire/serverless/functions?tabs=dotnet-cli&pivots=visual-studio#add-azure-functions-resource
 builder.AddAzureFunctionsProject<Projects.FunctionApp1>("functionapp1")
     .WithExternalHttpEndpoints()
@@ -17,6 +28,11 @@ builder.AddAzureFunctionsProject<Projects.FunctionApp1>("functionapp1")
     .WithReference(blobs)
     .WithReference(queues)
     .WithReference(tables)
+    .WithReference(secrets)
+    .WithReference(openai)
+    .WithReference(cosmosdb)
+    .WithReference(search)
+    .WithReference(webPubSub)
     ;
 
 builder.AddAzureFunctionsProject<Projects.DurableFunctionApp1>("durablefunctionapp1");
