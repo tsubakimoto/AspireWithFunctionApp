ac61bda94bc3fc9234f44a907b007c29c128fce1
Yuta Matsumura <tkmbmbr@gmail.com>
2025-08-16 14:41:07 +0900

    update chatservice

--- AzureAIFoundryFunctionApp/ChatService.cs
+++ AzureAIFoundryFunctionApp/ChatService.cs
@@ -1,24 +1,6 @@
 using Microsoft.Extensions.AI;
-//using OpenAI;
-//using OpenAI.Chat;
 
 namespace AzureAIFoundryFunctionApp;
-
-//public class ChatService(OpenAIClient client)
-//{
-//    public async Task<string> GetChatResponseAsync(string prompt)
-//    {
-//        var chatClient = client.GetChatClient("phi-3.5-mini");
-
-//        var response = await chatClient.CompleteChatAsync(
-//            new[]
-//            {
-//                new UserChatMessage(prompt)
-//            });
-
-//        return response.Value.Content[0].Text;
-//    }
-//}
 
 public class ChatService(IChatClient chatClient)
 {
